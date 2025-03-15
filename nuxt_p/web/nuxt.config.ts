b2e59b155e854b091172c1f086444d51f1d411bb
RintaroSUGIOKA <rintaro.sugioka@beyondsight.co.jp>
2025-03-16 08:21:03 +0900

    prettier導入

--- nuxt_p/web/nuxt.config.ts
+++ nuxt_p/web/nuxt.config.ts
@@ -1,9 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: "2024-11-01",
+  compatibilityDate: '2024-11-01',
   css: ['~/assets/styles/global.scss'],
   devtools: { enabled: false },
-  modules: ["@nuxt/eslint"],
+  modules: ['@nuxt/eslint'],
   ssr: false, // SPAモードで実行
   typescript: {
     typeCheck: true, // ビルド時・開発時に型チェック
