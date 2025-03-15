a3ecc1961fc939bcae5da60d15e21536ce7d7d0e
RintaroSUGIOKA <rintaro.sugioka@beyondsight.co.jp>
2025-03-16 08:13:24 +0900

    normalized.css導入、global.scssを適用してプロジェクト規定のスタイルを適用

--- nuxt_p/web/nuxt.config.ts
+++ nuxt_p/web/nuxt.config.ts
@@ -1,6 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
+  css: ['~/assets/styles/global.scss'],
   devtools: { enabled: false },
   modules: ["@nuxt/eslint"],
   ssr: false, // SPAモードで実行
